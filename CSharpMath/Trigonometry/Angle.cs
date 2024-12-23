e8d2817f18cd01f0d1dfbfc173a74ef124e9e533
Jeffrey Laederach <jefflae@outlook.com>
2024-12-22 23:40:12 -0500

    Added Angle Conversions

---
+++ CSharpMath/Trigonometry/Angle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMath.Trigonometry
+{
+    public class Angle
+    {
+        public static double RadiansToDegrees(double radians)
+        {
+            double degrees = (180 / Math.PI) * radians;
+            return degrees;
+        }
+
+        public static double DegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return radians;
+        }
+    }
+}
